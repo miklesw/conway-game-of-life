08df43bce864869fed2a62e43618b62da717900b
miklesw <miklesw@gmail.com>
2018-08-07 12:20:52 +0800

    Addressing grid concurrency concerns.

--- src/main/java/com/miklesw/conway/grid/GridManager.java
+++ src/main/java/com/miklesw/conway/grid/GridManager.java
@@ -4,16 +4,22 @@ import com.google.common.collect.ImmutableMap;
 import com.miklesw.conway.grid.events.GridEventPublisher;
 import com.miklesw.conway.utils.ColorUtils;
 
-import java.awt.Color;
+import java.awt.*;
 import java.util.*;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import static com.miklesw.conway.grid.util.GridUtils.findNeighbouringCells;
 import static java.util.stream.Collectors.toList;
 
+
 public class GridManager {
 
     private Map<CellPosition, CellState> grid;
+
+    private final Lock gridState = new ReentrantLock();
 
     private final GridEventPublisher gridEventPublisher;
 
@@ -21,11 +27,12 @@ public class GridManager {
 
     private final int gridSizeY;
 
+
     public GridManager(GridEventPublisher gridEventPublisher, int gridSizeX, int gridSizeY) {
         this.gridEventPublisher = gridEventPublisher;
         this.gridSizeX = gridSizeX;
         this.gridSizeY = gridSizeY;
-        generateGrid(gridSizeX, gridSizeY);
+        generateGrid();
     }
 
     public Map<CellPosition, CellState> getGrid() {
@@ -33,46 +40,63 @@ public class GridManager {
     }
 
     public void spawnCell(CellPosition position, Color color) {
-        CellState currentState = grid.get(position);
+        gridState.lock();
 
-        if (currentState.isLive()) {
-            String message = String.format("Cell at %s is already live with colour %s!", position, currentState.getColor());
-            throw new IllegalStateException(message);
+        try {
+            CellState currentState = grid.get(position);
+
+            if (currentState.isLive()) {
+                String message = String.format("Cell at %s is already live with colour %s!", position, currentState.getColor());
+                throw new IllegalStateException(message);
+            }
+
+            CellState liveState = CellState.live(color);
+            grid.put(position, liveState);
+            gridEventPublisher.publishCellChangedEvent(position, liveState);
+        } finally {
+            gridState.unlock();
         }
-
-        CellState liveState = CellState.live(color);
-        grid.put(position, liveState);
-        gridEventPublisher.publishCellChangedEvent(position, liveState);
     }
 
     private void killCell(CellPosition position) {
-        grid.put(position, CellState.dead());
-        gridEventPublisher.publishCellChangedEvent(position, CellState.dead());
+        gridState.lock();
+
+        try {
+            grid.put(position, CellState.dead());
+            gridEventPublisher.publishCellChangedEvent(position, CellState.dead());
+        } finally {
+            gridState.unlock();
+        }
     }
 
     public void nextState() {
         Set<CellPosition> cellsToKill = new HashSet<>();
         Map<CellPosition, Color> cellsToSpawn = new HashMap<>();
 
-        // determine next state
-        for (Map.Entry<CellPosition, CellState> gridEntry : this.grid.entrySet()) {
-            CellPosition cellPosition = gridEntry.getKey();
-            CellState cellState = gridEntry.getValue();
+        gridState.lock();
+        try {
+            // determine next state
+            for (Map.Entry<CellPosition, CellState> gridEntry : this.grid.entrySet()) {
+                CellPosition cellPosition = gridEntry.getKey();
+                CellState cellState = gridEntry.getValue();
 
-            List<CellState> liveNeighbours = findLiveNeighbourCellStates(cellPosition);
-            int liveNeighbourCount = liveNeighbours.size();
+                List<CellState> liveNeighbours = findLiveNeighbourCellStates(cellPosition);
+                int liveNeighbourCount = liveNeighbours.size();
 
-            if (cellState.isLive() && (liveNeighbourCount < 2 || liveNeighbourCount > 3)) {
-                cellsToKill.add(cellPosition);
-            } else if (!cellState.isLive() && liveNeighbourCount == 3) {
-                cellsToSpawn.put(cellPosition, findAverageCellColor(liveNeighbours));
+                if (cellState.isLive() && (liveNeighbourCount < 2 || liveNeighbourCount > 3)) {
+                    cellsToKill.add(cellPosition);
+                } else if (!cellState.isLive() && liveNeighbourCount == 3) {
+                    cellsToSpawn.put(cellPosition, findAverageCellColor(liveNeighbours));
+                }
             }
-        }
 
-        // apply state
-        cellsToKill.forEach(this::killCell);
-        // TODO: handling of exception?
-        cellsToSpawn.forEach(this::spawnCell);
+            // apply state
+            cellsToKill.forEach(this::killCell);
+            cellsToSpawn.forEach(this::spawnCell);
+
+        } finally {
+            gridState.unlock();
+        }
     }
 
     private Color findAverageCellColor(List<CellState> cellStates) {
@@ -89,7 +113,7 @@ public class GridManager {
                 .collect(toList());
     }
 
-    private void generateGrid(int gridSizeX, int gridSizeY) {
+    private void generateGrid() {
         this.grid = new ConcurrentHashMap<>();
         for (int x = 1; x <= gridSizeX; x++) {
             for (int y = 1; y <= gridSizeY; y++) {
